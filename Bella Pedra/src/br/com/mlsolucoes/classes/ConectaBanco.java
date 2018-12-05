9a716aa0489c772f8d77fd05a5efa18d79272f7c
WLADIMIR\wladi <wladimirsouza@outlook.com>
2018-12-04 23:02:02 -0200

    Primeiro commit e tela de login feita.

---
+++ Bella Pedra/src/br/com/mlsolucoes/classes/ConectaBanco.java
@@ -0,0 +1,25 @@
+package br.com.mlsolucoes.classes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JOptionPane;
+
+public class ConectaBanco {
+	
+	public Connection con;
+	public static Statement stm;		
+	
+	public void conectaBanco() throws SQLException {
+		
+		try{
+		con = DriverManager.getConnection("jdbc:mariadb://localhost:3306/baseconhecimento","root","root");
+		stm = con.createStatement();
+		}catch(SQLException e){
+			JOptionPane.showMessageDialog(null, "Não Foi Possível Conectar ao Banco");
+		}
+	}	
+
+}
---
+++ Bella Pedra/src/br/com/mlsolucoes/telas/TelaLogin.java
@@ -0,0 +1,135 @@
+package br.com.mlsolucoes.telas;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JButton;
+
+public class TelaLogin extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaLogin frame = new TelaLogin();
+					frame.setVisible(true);
+					frame.setResizable(false);
+					frame.setLocationRelativeTo(null);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaLogin() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 660, 520);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 658, 488);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JButton btnOk = new JButton("OK");
+		btnOk.setIcon(new ImageIcon(TelaLogin.class.getResource("/br/com/mlsolucoes/imagens/sign-check-icon.png")));
+		btnOk.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		btnOk.setBounds(368, 401, 112, 41);
+		panel.add(btnOk);
+		
+		JButton btnSair = new JButton("Sair");
+		btnSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		btnSair.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		btnSair.setIcon(new ImageIcon(TelaLogin.class.getResource("/br/com/mlsolucoes/imagens/sign-delete-icon.png")));
+		btnSair.setBounds(499, 401, 112, 41);
+		panel.add(btnSair);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(299, 210, 202, 31);
+		panel.add(passwordField);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 649, 26);
+		panel.add(menuBar);
+		
+		JMenu mnArquivo = new JMenu("Arquivo");
+		mnArquivo.setFont(new Font("Segoe UI", Font.PLAIN, 18));
+		menuBar.add(mnArquivo);
+		
+		JMenuItem mntmNovoUsurio = new JMenuItem("Novo Usu\u00E1rio");
+		mntmNovoUsurio.setFont(new Font("Segoe UI", Font.PLAIN, 17));
+		mnArquivo.add(mntmNovoUsurio);
+		
+		JMenuItem mntmSair = new JMenuItem("Sair");
+		mntmSair.setFont(new Font("Segoe UI", Font.PLAIN, 17));
+		mntmSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+		});
+		mnArquivo.add(mntmSair);
+		
+		JMenu mnSobre = new JMenu("Sobre");
+		mnSobre.setFont(new Font("Segoe UI", Font.PLAIN, 18));
+		menuBar.add(mnSobre);
+		
+		JMenuItem mntmSobreAAplicao = new JMenuItem("Sobre a Aplica\u00E7\u00E3o");
+		mntmSobreAAplicao.setFont(new Font("Segoe UI", Font.PLAIN, 17));
+		mnSobre.add(mntmSobreAAplicao);
+		
+		JLabel lblUsurio = new JLabel("Usu\u00E1rio");
+		lblUsurio.setForeground(Color.DARK_GRAY);
+		lblUsurio.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblUsurio.setBounds(123, 127, 112, 16);
+		panel.add(lblUsurio);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		textField.setBounds(299, 115, 202, 31);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblSenha = new JLabel("Senha");
+		lblSenha.setForeground(Color.DARK_GRAY);
+		lblSenha.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblSenha.setBounds(123, 222, 90, 16);
+		panel.add(lblSenha);
+		
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(TelaLogin.class.getResource("/br/com/mlsolucoes/imagens/login.jpg")));
+		label.setBounds(0, 26, 649, 462);
+		panel.add(label);
+	}
+}
